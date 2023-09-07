278d3f74c79e133d7dd03e73b3ea6a1c103be8de
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-09-07 10:49:07 -0400

    create borderMsg function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("Hello, World!");
+
+function borderMsg(msg) {
+    const border = "=".repeat(msg.length);
+
+    console.log(border);
+    console.log(msg);
+    console.log(border);
+}
+
+borderMsg("Just Testing This Function");
